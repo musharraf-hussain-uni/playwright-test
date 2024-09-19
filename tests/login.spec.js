d8fdc7cb8c0d03803c8f479aca2e2208bc6c336c
Musharraf Hussain <nabeelhussain056@gmail.com>
2024-09-19 11:50:52 +0500

    describe and beforeEach

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,62 +3,51 @@ const {
   expect
 } = require('@playwright/test');
 
-test('To test the login page', async ({
-  page
-}) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+test.describe('OrangeHRM Login Tests', () => {
+  test.beforeEach(async ({
+    page
+  }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  });
 
-  await page.fill('input[name="username"]', 'Admin');
-  //await page.locator('input[name="username"]').type('Admin');
+  test('Positive test for valid login', async ({
+    page
+  }) => {
+    await page.fill('input[name="username"]', 'Admin');
+    await page.fill('input[name="password"]', 'admin123');
 
-  await page.fill('input[name="password"]', 'admin123');
-  //await page.locator('input[name="password"]').type('admin123');
+    await page.click("button[type='submit']");
 
+    await page.waitForTimeout(3000);
 
-  await page.click("button[type='submit']");
-  //await page.locator("button[type='submit']").click();
+    await expect(page).toHaveURL(/dashboard/);
 
+    await expect(page.locator('h6')).toHaveText('Dashboard');
 
-  await page.waitForTimeout(3000);
+    await page.getByAltText("profile picture").first().click();
+    await page.getByText("Logout").click();
 
-  await expect(page).toHaveURL(/dashboard/);
+    await page.waitForTimeout(3000);
 
-  await expect(page.locator('h6')).toHaveText('Dashboard');
+    await expect(page).toHaveURL(/login/);
+  });
 
+  test('Negative test for invalid login', async ({
+    page
+  }) => {
+    await page.fill('input[name="username"]', 'InvalidUser');
+    await page.fill('input[name="password"]', 'wrongpassword');
 
-  await page.getByAltText("profile picture").first().click();
+    await page.click('button[type="submit"]');
 
-  await page.getByText("Logout").click()
+    await page.waitForSelector('.oxd-alert-content');
 
-  await page.waitForTimeout(3000);
+    await expect(page).not.toHaveURL(/dashboard/);
 
-  await expect(page).toHaveURL(/login/);
+    const errorMessage = await page.locator('.oxd-alert-content');
+    await expect(errorMessage).toBeVisible();
+    await expect(errorMessage).toHaveText('Invalid credentials');
 
-});
-
-
-test.only('Negative test for invalid login', async ({
-  page
-}) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-
-  await page.fill('input[name="username"]', 'InvalidUser');
-
-  await page.fill('input[name="password"]', 'wrongpassword');
-
-  await page.click('button[type="submit"]');
-
-  await page.waitForSelector('.oxd-alert-content');
-
-  await expect(page).not.toHaveURL(/dashboard/);
-
-  const errorMessage = await page.locator('.oxd-alert-content');
-
-  await expect(errorMessage).toBeVisible();
-
-  await expect(errorMessage).toHaveText('Invalid credentials');
-
-  await page.waitForTimeout(3000);
-
-
+    await page.waitForTimeout(3000);
+  });
 });
